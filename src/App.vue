e23bc3014df0269ec91ca499ef07c29b6b42834b
Vejber <105496392+Vejber@users.noreply.github.com>
2023-08-19 16:28:56 +0300

    базовые функции Vue

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+// здесь описывать наш компонент
+
+<template>
+    //внутри - разметка, шаблон для страницы
+    <div>
+        It freaking works
+
+        <button @click="addLike">
+            Like
+        </button>
+        <button @click="addDislike">
+            Dislike
+        </button>
+        <div> likes count: <strong>{{ likes }}</strong> </div>
+        <div> dislikes count: <strong>{{ dislikes }}</strong> </div>
+    </div>
+</template>
+
+<script>
+//внутри - логика компонента
+export default { //обязательная строка
+    data() {
+        return {
+            likes: 0,
+            dislikes: 5,
+        }
+    },
+
+    methods:{
+        addLike(){
+            this.likes += 1;
+        },
+        addDislike(){
+            this.dislikes += 1;
+        }
+    }
+}
+
+</script>
+
+<style></style>
