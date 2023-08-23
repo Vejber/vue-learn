0be5055ec313c72f34b255d86ca451f8d32c0356
Vejber <105496392+Vejber@users.noreply.github.com>
2023-08-23 12:55:49 +0300

    trying

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
+import App from './Header-template.vue'
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+
+createApp(App).mount('#header-id')
